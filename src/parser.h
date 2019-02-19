d48ba4bfc5261f7b9dd5a77f1abf755f0d859b35
Permint <lluc123@git.com>
2019-02-19 16:30:36 -0500

    temps stufff

--- src/parser.h
+++ src/parser.h
@@ -4,17 +4,33 @@
 #include <string.h>
 #include <stdbool.h>
 
-struct http_param {
+typedef struct {
     char *pname;
     char *pvalue;
-};
+} http_param, *phttp_param;
+
+typedef struct {
+	node_list_param * next;
+	http_param value;
+} node_list_param;
+
+typedef struct {
+	node_list_param * next;
+	node_list_param * last;
+	size_t len;
+} list_param;
 
 extern const struct http_param l_error_param;
 
 bool paramIsNotNull(const struct http_param p);
-bool paramComp(const struct http_param p,const struct http_param d);
-int init_param(char * key, char * value, struct http_param* p);
-int destroy_param(struct http_param p);
-int clear_param(struct http_param* p);
-struct http_param* parser(char *buffer, size_t s, char separator, char setter);
+bool paramComp(const http_param p,const http_param d);
+int init_param(char * key, char * value, phttp_param p);
+int destroy_param(http_param p);
+int clear_param(phttp_param p);
+phttp_param parser(char *buffer, size_t s, char separator, char setter);
+
+list_param list_param_init();
+int list_param_addlast(list_param * obj, http_param val);
+int list_param_at(list_param * obj, int index);
+int list_param_free(list_param * obj);
 #endif
